3850618455e688c14ca2aa3f044ffc96eb849289
Sominhwan <thalsghks@naver.com>
2023-03-23 00:38:40 +0900

    d-pay 버튼 추가

--- simple_practice/lib/pages/dinnerInfoPage.dart
+++ simple_practice/lib/pages/dinnerInfoPage.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 import '../main.dart';
 
-class DinnerInfoPage extends StatelessWidget {
+class DinnerInfoPage extends StatefulWidget {
+  const DinnerInfoPage({Key? key}) : super(key: key);
+
+  @override
+  DPayInfo createState() => DPayInfo();
+}
+
+class DPayInfo extends State<DinnerInfoPage>{
+  List<String> _toggleList = ['후불 정산 내역', 'D-Pay 사용내역'];
+  List<bool> _isSelected = [false, false];
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+        backgroundColor: Color.fromRGBO(242, 242, 242, 100),
         appBar: PreferredSize(
           preferredSize: Size.fromHeight(70.0),
           child: AppBar(
             backgroundColor: Color.fromRGBO(36, 87, 189, 1.0),
             leading: Padding(
               padding: const EdgeInsets.only(left: 12),
-              child: Row(crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Image.asset('assets/images/deulogo.png',height: 45),
-                ],
-              ),
+                child: Row(crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Image.asset('assets/images/deulogo.png',height: 45),
+                 ],
+                ),
             ), // 이미지 경로 설정
             title: const Text("식수 사용내역",
               style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                  height: 2
+               fontSize: 30.0,
+               fontWeight: FontWeight.bold,
+               height: 2
               ),
             ),
             actions: <Widget>[
@@ -43,10 +54,35 @@ class DinnerInfoPage extends StatelessWidget {
             elevation: 0.0,
           ),
         ),
-        body: Container(
 
-        )
+        body: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10, left: 23),
+            child: Container(
+              height: 35,
+              child: ToggleButtons(
+                borderRadius: BorderRadius.all(Radius.circular(6)),
+                fillColor: Colors.blue,
+                selectedColor: Colors.white,
+                children: _toggleList.map((String value) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 50, right: 50),
+                    child: Text(
+                      value,
+                      style: TextStyle(fontSize: 13),
+                    ),
+                  );
+                }).toList(),
+                isSelected: _isSelected,
+                onPressed: (int index) {
+                  setState(() {
+                    _isSelected[index] = !_isSelected[index];
+                  });
+                },
+              ),
+            ),
+          ),
+        ),
     );
   }
-
 }
